c6127ddf446bd701055989b08a0c59aa03ba22e8
Harry Zhang <hzzh0301@gmail.com>
2019-07-05 16:48:39 -0700

    pod utils

--- pkg/api/v1/pod/util.go
+++ pkg/api/v1/pod/util.go
@@ -201,6 +201,27 @@ func visitContainerConfigmapNames(container *v1.Container, visitor Visitor) bool
 	return true
 }
 
+// IsOneOffPod determines if a pod is run-to-finish, this applied specifically to Pinterest
+func IsOneOffPod(pod *v1.Pod) bool {
+	for _, oRef := range pod.OwnerReferences {
+		// Use more strict checking to avoid false positive
+		if oRef.Kind == "TFJob" || oRef.Kind == "PyTorchJob" || oRef.Kind == "Job" {
+			return true
+		}
+	}
+	return false
+}
+
+// IsSideCar determines if a container is marked as sidecar container in Pinterest
+func IsSideCar(container *v1.Container) bool {
+	for _, envVar := range container.Env {
+		if envVar.Name == "IS_SIDE_CAR" && envVar.Value == "true" {
+			return true
+		}
+	}
+	return false
+}
+
 // GetContainerStatus extracts the status of container "name" from "statuses".
 // It also returns if "name" exists.
 func GetContainerStatus(statuses []v1.ContainerStatus, name string) (v1.ContainerStatus, bool) {
